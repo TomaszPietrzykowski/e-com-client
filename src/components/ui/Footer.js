99bae6c049e36202f25bc7944587c37792da8082
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-09-16 19:53:40 +0200

    footer finished

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,13 +18,20 @@ const useStyles = makeStyles((theme) => ({
   gridContainer: {
     width: "100%",
     display: "flex",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
+    flexWrap: "wrap",
+    marginBottom: "2rem",
   },
   column: {
     display: "flex",
     flexDirection: "column",
     padding: "2rem",
+    minWidth: 200,
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1rem",
+      padding: "1rem",
+    },
   },
   columnHeader: {
     fontFamily: "Bree Serif",
@@ -33,6 +42,41 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "Open Sans",
     fontSize: ".9rem",
     padding: ".3rem",
+    cursor: "pointer",
+    "&:hover": {
+      color: theme.palette.common.blue,
+    },
+    [theme.breakpoints.down("md")]: {
+      fontSize: ".8rem",
+    },
+  },
+  social: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "2rem",
+    position: "relative",
+    [theme.breakpoints.down("md")]: {
+      padding: "1rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: 0,
+    },
+    "&:before": {
+      content: '""',
+      width: "30%",
+      height: 1,
+      backgroundColor: "white",
+      position: "absolute",
+      top: 0,
+      [theme.breakpoints.down("md")]: {
+        width: "50%",
+      },
+    },
+  },
+  socialIcon: {
+    fontSize: "3rem",
+    margin: "1rem",
   },
 }));
 
@@ -63,6 +107,11 @@ const Footer = () => {
             <div className={classes.link}>Reklamacje i zwroty</div>
             <div className={classes.link}>Promocje</div>
           </div>
+        </div>
+
+        <div className={classes.social}>
+          <FacebookIcon className={classes.socialIcon} />
+          <InstagramIcon className={classes.socialIcon} />
         </div>
       </div>
     </div>
